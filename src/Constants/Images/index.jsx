f6dcdde38697081d0a2e78839da6ed6394b17804
Muhammad Waleed <mw051878@gmail.com>
2025-03-19 11:42:58 +0500

    setup

--- src/Constants/Images/index.jsx
+++ src/Constants/Images/index.jsx
@@ -1,10 +1,10 @@
-const bgISBImages = "/public/Bg_Isb_Airport.jpg";
-const logoIsbAirport = "/public/Logo_Isb_Airport.png";
+const bgISBImages = "/Airport-Parking/public/Bg_Isb_Airport.jpg";
+const logoIsbAirport = "/Airport-Parking/public/Logo_Isb_Airport.png";
 
 const Images = {
-    bgISBImages,
-    logoIsbAirport,
-    // Add more images as needed
-}
+  bgISBImages,
+  logoIsbAirport,
+  // Add more images as needed
+};
 
 export default Images;
